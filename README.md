6517c5a403bbdc7b56f3aa24a90c0cf87dbff08b
song song <ssong@arxanculture.com>
2018-06-25 18:50:53 +0800

    添加说明

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # php-common
 php-common is an implementation of the sdk-go-common in Php.
+
+该包主要提供ecc加密签名，解密验签，ed25519签名问题
--- cryption/sign.php
+++ cryption/sign.php
@@ -1,6 +1,6 @@
 <?php
 
-require (__DIR__ . "/../config.php")
+require (__DIR__ . "/../config.php");
 
 // ed25519签名
 class Signature{
